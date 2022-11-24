1a7c4bc32b9e53d7478a41f98bc78cedce0ce6c5
shea <zhushuai026@gmail.com>
2022-11-24 23:17:18 +0800

    feat: spring native

---
+++ spring-boot-3.x/spring-native/README.md
@@ -0,0 +1,6 @@
+- 打native包
+```shell
+sudo mvn -Pnative native:compile
+```
+
+
---
+++ spring-boot-3.x/spring-native/src/main/java/com/di1shuai/springnative/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.di1shuai.springnative.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String hello(){
+        return "GraalVM ...";
+    }
+}
